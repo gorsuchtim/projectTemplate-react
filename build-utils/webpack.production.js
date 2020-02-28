ce97f9f9e2c3fff737d9fa3f66b6d536151f9b48
Tim Gorsuch <gorsuchtim@gmail.com>
2020-02-27 19:07:03 -0600

    initial commit build-utils folder

---
+++ build-utils/webpack.production.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = () => {
+  output: {
+    //[chunkhash]
+    filename: "App.min.js";
+  }
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          minimize: true
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader?sourceMap"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        loader: "eslint-loader",
+        options: {
+          failOnError: true,
+          quiet: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ];
+  }
+};
