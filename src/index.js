8ba19ccbde59fb237444d3e99e6088e26cff920c
Tim Gorsuch <gorsuchtim@gmail.com>
2020-04-24 14:17:38 -0500

    set index to run app on DOM load

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import "./scss/shared.scss";
 import App from "./js/App";
 
 /*** Executed Code ***/
-console.log("hi from index js");
-//window.addEventListener("DOMContentLoaded", App);
+window.addEventListener("DOMContentLoaded", App);
