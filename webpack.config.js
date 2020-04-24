1df6399c83d3bde1ec9d3fcf603057ff22d1bd94
Tim Gorsuch <gorsuchtim@gmail.com>
2020-04-24 14:03:32 -0500

    add WP env files

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,22 @@
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpackMerge = require("webpack-merge");
 
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
-
-module.exports = ({ mode }) => {
-  return webpackMerge(
-    {
-      mode,
-      output: {
-        filename: "App.dev.js"
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "App.dev.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
-      devServer: {
-        open: true
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: "./src/html/index.html", // path to dev index.html
-          filename: "./index.html" // name for production build html
-        }),
-        new webpack.ProgressPlugin()
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.html$/,
-            loader: "html-loader",
-            options: {
-              minimize: true
-            }
-          },
-          {
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader?sourceMap:", "sass-loader"]
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-              {
-                loader: "url-loader",
-                options: {
-                  limit: 5000
-                }
-              }
-            ]
-          }
-        ]
-      }
-    },
-    modeConfig(mode)
-  );
+    ],
+  },
 };
