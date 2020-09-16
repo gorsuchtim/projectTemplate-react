09ee2b8df20c74ecc3e271c6f2f1a8422cfced5d
Tim <tgorsuch@redventures.com>
2020-09-16 12:11:09 -0400

    comments out import of component index

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import ComponentIndex from "./Components/ComponentIndex";
+// import ComponentIndex from "./Components/ComponentIndex";
 
 const App = () => {
   return (
