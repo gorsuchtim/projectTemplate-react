9fd2271bccc56ec4dd037c399585aa9c53a8114e
Tim <tgorsuch@redventures.com>
2020-09-16 12:10:37 -0400

    adds test, rmv old code

--- src/js/Components/Test/Test.js
+++ src/js/Components/Test/Test.js
@@ -1,12 +1,12 @@
 /**
- * ComponentTemplate
+ * Test
  * Copy and paste for each new component
  * Components require return { initialize } as this is what is ran from the ComponentIndex.js file
  */
 
 export default (() => {
   const handleTemplate = () => {
-    console.log("Im the component template");
+    console.log("Im test template!");
   };
   const initialize = () => {
     handleTemplate();
