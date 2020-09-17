ac54451ba423f5c71ae70d018b9cd7752a90c6c5
Tim <tgorsuch@redventures.com>
2020-09-17 15:39:26 -0400

    removes component index and template

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from "react";
-// import ComponentIndex from "./Components/ComponentIndex";
 
 const App = () => {
   return (
     <Fragment>
       <h1>Hi I am the template</h1>
       <h3>Hope you have fun</h3>
-
-      <div className="component-template"></div>
     </Fragment>
   );
 };
