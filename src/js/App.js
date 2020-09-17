30306d65f81ea7ad72bc3930837c68b2e32e123f
Tim <tgorsuch@redventures.com>
2020-09-17 15:51:58 -0400

    adds tailwind classes & custom classes as placeholder

--- src/js/App.js
+++ src/js/App.js
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const App = () => {
   return (
     <Fragment>
-      <div>
-        <h1>Hi I am the template</h1>
+      <div class="section  bg-blue--primary  flex flex-col justify-center items-center">
+        <h1 class="headline">Hi I am the template</h1>
         <h3>Hope you have fun</h3>
       </div>
     </Fragment>
