f551e9b5da8563aabbf72ac2fa94a64015a80a83
Tim <tgorsuch@redventures.com>
2020-12-07 10:52:43 -0500

    adds test component, runs npm audit fix

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Test from "./Test";
 
 const App = () => {
   return (
-    <Fragment>
-      <div className="section  bg-blue--primary  flex flex-col justify-center items-center">
-        <h1 className="headline">Hi I am the template</h1>
-        <h3>Hope you have fun</h3>
-      </div>
-    </Fragment>
+    <div className="section  bg-blue--primary  flex flex-col justify-center items-center">
+      <h1 className="headline">Hi I am the template</h1>
+      <h3>Hope you have fun</h3>
+    </div>
   );
 };
 
---
+++ src/js/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <h1>I am the test component!</h1>
+    </div>
+  );
+};
+
+export default Test;
