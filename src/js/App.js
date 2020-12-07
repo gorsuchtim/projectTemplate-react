26c038af38d287e7c9fe6e0d6f0b3b959667d6d3
Tim <tgorsuch@redventures.com>
2020-12-07 10:18:54 -0500

    updates jsx "class" to "className"

--- src/js/App.js
+++ src/js/App.js
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const App = () => {
   return (
     <Fragment>
-      <div class="section  bg-blue--primary  flex flex-col justify-center items-center">
-        <h1 class="headline">Hi I am the template</h1>
+      <div className="section  bg-blue--primary  flex flex-col justify-center items-center">
+        <h1 className="headline">Hi I am the template</h1>
         <h3>Hope you have fun</h3>
       </div>
     </Fragment>
