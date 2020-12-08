dada8fc76963d156b31250d68dc0ed79dc42bea9
Tim <tgorsuch@redventures.com>
2020-12-08 10:41:01 -0500

    deletes test component

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Test from "./Components/Test/Test";
 
 const App = () => {
   return (
     <div className="section  bg-blue--primary  flex flex-col justify-center items-center">
       <h1 className="headline">Hi I am the template</h1>
       <h3>Hope you have fun</h3>
-
-      <Test />
     </div>
   );
 };
