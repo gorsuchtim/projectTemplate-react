a11ba1871b7f2898f9fc006f75a46f20fa187c93
Tim <tgorsuch@redventures.com>
2021-02-04 16:15:13 -0500

    updates babelrc, eslintrc

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="section  bg-blue--primary  flex flex-col justify-center items-center">
+    <div className="p-48  bg-blue--primary  flex flex-col justify-center items-center">
       <h1 className="headline">Hi I am the template</h1>
       <h3>Hope you have fun</h3>
     </div>
